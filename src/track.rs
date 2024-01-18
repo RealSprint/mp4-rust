89c37b63a2070cd6a15521ef633a8c33a231f128
Mattias Bergström <mattias.bergstrom@outlook.com>
2024-01-18 08:51:15 +0100

    Add method to get media config

--- src/track.rs
+++ src/track.rs
@@ -202,6 +202,71 @@ impl Mp4Track {
         }
     }
 
+    pub fn media_config(&self) -> Result<MediaConfig> {
+        match self.media_type()? {
+            MediaType::H264 => Ok(MediaConfig::AvcConfig(AvcConfig {
+                width: self.width(),
+                height: self.height(),
+                seq_param_set: self.sequence_parameter_set()?.to_vec(),
+                pic_param_set: self.picture_parameter_set()?.to_vec(),
+                color: self.color_config().cloned(),
+                aspect_ratio: self.aspect_ratio(),
+            })),
+            MediaType::H265 => Ok(MediaConfig::HevcConfig(HevcConfig {
+                width: self.width(),
+                height: self.height(),
+            })),
+            MediaType::AV1 => {
+                if let Some(av01) = self.trak.mdia.minf.stbl.stsd.av01.as_ref() {
+                    let av1c = &av01.av1c;
+
+                    return Ok(MediaConfig::Av1Config(Av1Config {
+                        width: self.width(),
+                        height: self.height(),
+                        color: self.color_config().cloned(),
+                        aspect_ratio: self.aspect_ratio(),
+                        sequence_header: av1c.sequence_header.clone(),
+                        tier: av1c.tier,
+                        profile: av1c.profile,
+                        level_idx: av1c.level_idx,
+                        bit_depth: av1c.bit_depth,
+                        monochrome: av1c.monochrome,
+                        subsampling_x: av1c.subsampling_x,
+                        subsampling_y: av1c.subsampling_y,
+                        chroma_sample_position: av1c.chroma_sample_position,
+                        initial_presentation_delay_minus_one: av1c
+                            .initial_presentation_delay_minus_one,
+                    }));
+                }
+                Err(Error::BoxNotFound(BoxType::Av01Box))
+            }
+            MediaType::VP9 => Ok(MediaConfig::Vp9Config(Vp9Config {
+                width: self.width(),
+                height: self.height(),
+            })),
+            MediaType::AAC => Ok(MediaConfig::AacConfig(AacConfig {
+                bitrate: self.bitrate(),
+                profile: self.audio_profile()?,
+                freq_index: self.sample_freq_index()?,
+                chan_conf: self.channel_config()?,
+            })),
+            MediaType::OPUS => {
+                if let Some(opus) = self.trak.mdia.minf.stbl.stsd.opus.as_ref() {
+                    return Ok(MediaConfig::OpusConfig(OpusConfig {
+                        bitrate: self.bitrate(),
+                        pre_skip: opus.dops.pre_skip,
+                        sample_rate: opus.dops.input_sample_rate,
+                        output_gain: opus.dops.output_gain,
+                        channel_mapping_family: opus.dops.channel_mapping_family.clone(),
+                    }));
+                }
+
+                Err(Error::BoxNotFound(BoxType::OpusBox))
+            }
+            MediaType::TTXT => Ok(MediaConfig::TtxtConfig(TtxtConfig {})),
+        }
+    }
+
     pub fn color_config(&self) -> Option<&ColorConfig> {
         let stst = &self.trak.mdia.minf.stbl.stsd;
         let color_config = match (&stst.av01, &stst.avc1) {
