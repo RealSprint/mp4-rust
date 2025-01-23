82f4efc8c3411861458a3a90dcd6fb8810e0b577
Kim Nilsson <kim.nilsson@realsprint.com>
2025-01-23 13:29:05 +0100

    Add test for init vector

--- src/encryption/initialization_vector.rs
+++ src/encryption/initialization_vector.rs
@@ -6,9 +6,6 @@ pub struct InitializationVector {
     pub(crate) data: [u8; 16],
 }
 
-// TODO: Double check if this is writter correctly - ISO/IEC 23001-7:2023(E) - 9.2
-// If the Per_Sample_IV_Size field is 8, then the 128-bit IV value is made of
-// InitializationVector value copied to bytes 0 to 7 and the bytes 8 to 15 are set to zero.
 impl InitializationVector {
     pub fn new_64_bit(data: [u8; 8]) -> Self {
         let mut iv = [0; 16];
@@ -19,5 +16,29 @@ impl InitializationVector {
 
     pub fn new_128_bit(data: [u8; 16]) -> Self {
         InitializationVector { size: 16, data }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_64_bit() {
+        let iv = InitializationVector::new_64_bit([1, 2, 3, 4, 5, 6, 7, 8]);
+        assert_eq!(iv.size, 8);
+        assert_eq!(iv.data, [1, 2, 3, 4, 5, 6, 7, 8, 0, 0, 0, 0, 0, 0, 0, 0]);
+    }
+
+    #[test]
+    fn test_new_128_bit() {
+        let iv = InitializationVector::new_128_bit([
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+        ]);
+        assert_eq!(iv.size, 16);
+        assert_eq!(
+            iv.data,
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,]
+        );
     }
 }
